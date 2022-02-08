083a852d80f9c5d6682069804a333755353e1c36
SayakaMizote <mizou.3d@gmail.com>
2022-02-09 06:02:20 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 溝手彩加 on 2022/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
